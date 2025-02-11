e28ee2f3d07fe2b55c9619951b46def66b6fca04
sva1pone <kiker.flysik@mail.ru>
2025-02-11 22:23:25 +0300

    Загрузка файлов

--- JavaLab1/src/Main.java
+++ JavaLab1/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+    public static void print_menu() {
+        String text = """
+1 - Шифрование с помощью операции XOR
+2 - Шифрование с помощью  циклического сдвига
+Q - Выход из программы
+""";
+        System.out.print(text);
+    }
+
+    public static String get_command() {
+        String command = System.in.read();
+        return command;
+    }
+
     public static void main(String[] args) {
         System.out.print("Hello word!");
+        print_menu();
+        get_command();
     }
 }
