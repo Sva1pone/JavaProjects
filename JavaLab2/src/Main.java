ebef30fbf04a0acf5154dda996358187f56201b9
Sva1pone <kiker.flysik@mail.ru>
2025-02-12 11:25:22 +0300

    Загрузка файлов

--- JavaLab2/src/Main.java
+++ JavaLab2/src/Main.java
@@ -3,6 +3,20 @@
 
 
 public class Main {
+    public static void print_menu() {
+        String text = """
+1 - Шифрование с помощью операции XOR
+2 - Шифрование с помощью  циклического сдвига
+Q - Выход из программы
+""";
+        System.out.print(text);
+    }
+
+    public static String get_command() {
+        String command = System.in.read();
+        return command;
+    }
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
