2f4e34c5eb7a67dea4efafab7f38dd6ce207dc70
Sva1pone <kiker.flysik@mail.ru>
2025-02-12 19:24:46 +0300

    Загрузка файлов

--- JavaLab2/src/Main.java
+++ JavaLab2/src/Main.java
@@ -1,5 +1,30 @@
 
 public class Main {
+    public static final String KEY = "sekret"
+
+    public static void encrypt_xor() {
+        System.out.print('Введите текст для шифрования:');
+        String original_text = System.in.read();
+        String encrypted_text;
+        String encrypted_char;
+        String original_char;
+        String key_char;
+
+        int index = 0;
+        for (int i = 0; i < original_text.length(); i++) {
+            original_char = Integer.toBinaryString(original_text[i]);
+            if (i >= KEY.length()) {
+                index = 0
+            }
+            key_char = Integer.toBinaryString(original_text[i])
+            encrypted_char = Integer.toBinaryString(original_text[i]);
+            encrypted_text += encrypted_char;
+
+            index++;
+        }
+    }
+
+
     public static void print_menu() {
         String text = """
 1 - Шифрование с помощью операции XOR
@@ -9,15 +34,20 @@ Q - Выход из программы
         System.out.print(text);
     }
 
-    public static String get_command() {
-        String command = System.in.read();
-        return command;
-    }
-
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        menu();
-
+        do {
+            print_menu();
+            String command = System.in.read();
+            switch (command) {
+                case '1':
+                    null
+                case '2':
+                    null
+                case 'Q':
+                    break;
+                default:
+                    System.out.print('Команда не распознана!');
+            }
+        } while (command != 'Q')
     }
 }
